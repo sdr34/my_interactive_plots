8093f636a3a21df99f89e33158128e32f7d1bdbe
Dmitry <dmgorschkov@mail.ru>
2024-11-11 17:41:26 +0300

    Bump version to 0.1.0 and add new features

--- setup.py
+++ setup.py
@@ -1,23 +1,22 @@
 from setuptools import setup, find_packages # type: ignore
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='my_interactive_plots',
     version='0.1.0',
     packages=find_packages(),
-    install_requires=[
-        'pandas',
-        'plotly',
-        'click'
+    include_package_data=True,
+   install_requires=[
+        'pandas>=1.0.0',
+        'plotly>=5.0.0',
+        'click>=8.0.0',
     ],
     extras_require={
         'dev': [
             'pytest',
             'click',
-        ],
-    },
-    entry_points={
-        'console_scripts': [
-            'myplot=my_interactive_plots.cli:cli'
         ],
     },
     author='Dmitry Gorschkov',
@@ -27,6 +26,13 @@ setup(
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'myplot=my_interactive_plots.cli:cli'
+        ],
+    },
 )
 
